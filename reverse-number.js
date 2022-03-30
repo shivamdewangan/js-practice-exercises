12c298c4a7a824d764445d2a2115c1fd00c9f8a7
Shivam Dewangan <shivamsubam@gmail.com>
2022-03-30 09:56:22 +0530

    Reverse number.
    Returns inverted number.

---
+++ reverse-number.js
@@ -0,0 +1,18 @@
+function reverse( number ) {
+  // Convert integer to string
+  number += '';
+  // Declare an empty result variable
+  var result = '';
+  // Loop through number length
+  for (var i = 0; i < number.length; i++) {
+    // Add to result in reverse order
+    result += number.charAt( ( number.length - 1 - i ) );
+  }
+  // Parse string to integer else return undefined
+  return parseFloat( result ) || undefined;
+};
+
+// Test
+reverse( 123456 ); // Result 654321
+reverse( 568954 ); // Result 459865
+reverse( 12345.1045 ); // Result 5401.54321
