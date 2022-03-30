18111b6b18082bdf905e15d8971402f2b563ecac
Shivam Dewangan <shivamsubam@gmail.com>
2022-03-30 09:46:03 +0530

    Finds even and odd number length in an Array.
    Returns an object of even and odd number length.

--- even-odd-in-array.js
+++ even-odd-in-array.js
@@ -1,16 +1,21 @@
 function evenOdd( array ) {
+  // Declare empty even and odd arrays
   var even = [] , odd = [];
+  // Loop through passed array
   for (var i = 0; i < array.length; i++) {
+    // Check number is even or not and push in respective arrays
     if ( array[i] % 2 === 0 ) {
       even.push( array[i] );
     } else {
       odd.push( array[i] );
     }
   }
+  // Return even and odd number length
   return {
-    even : even,
-    odd : odd,
-    evenCount : even.length,
-    oddCount : odd.count,
+    even : even.length,
+    odd : odd.length,
   };
 };
+
+// Test
+evenOdd([ 1 , 4 , 6 , 7 , 5 , 2 , 3 ]); //Result {even: 3, odd: 4}
